c2be9b3d7dd20e1399d8fb4864dadccc72765cde
Isiah Jones <jones.isiah.blhs@gmail.com>
2022-10-15 15:35:35 -0400

    Configure delete action

--- app.vue
+++ app.vue
@@ -27,6 +27,12 @@ const computeSelectedNames = () => {
   selectedNames.value = filteredNames.map((name) => name.name);
 };
 
+const removeName = (index: number) => {
+  const filteredNames = [...selectedNames.value];
+  filteredNames.splice(index, 1);
+  selectedNames.value = filteredNames;
+};
+
 const optionsArray = [
   {
     title: "1) Choose a gender",
@@ -64,7 +70,13 @@ const optionsArray = [
       </button>
     </div>
     <div class="cards-container">
-      <CardName v-for="name in selectedNames" :key="name" :name="name" />
+      <CardName
+        v-for="(name, index) in selectedNames"
+        :key="name"
+        :name="name"
+        @remove="() => removeName(index)"
+        :index="index"
+      />
     </div>
   </div>
 </template>
